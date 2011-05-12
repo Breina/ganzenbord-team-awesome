4d0ab8a5b2d3afc225864620cd6d56d5f50007eb
brecht_derwael@msn.com <brecht_derwael@msn.com@eddbf101-9bcb-924f-d48c-6b5aac6e24a5>
2011-05-12 12:57:15 +0000

    De structuur van de vakskes uitgelegd.

---
+++ trunk/Project challenge ganzenbord/Tile.vb
@@ -0,0 +1,42 @@
+﻿'Brecht
+Public Class Tile
+    Protected _x, _y, _id As Integer  'x en y zijn cordinaten om te tekenen, id is hoe ver het zich bevind in het veld, 0 is de start
+
+    Public ReadOnly Property X() As Integer
+        Get
+            Return _x
+        End Get
+    End Property
+
+    Public ReadOnly Property Y() As Integer
+        Get
+            Return _y
+        End Get
+    End Property
+
+    Public Sub New(ByVal x As Integer, ByVal y As Integer, ByVal id As Integer)
+        _x = x
+        _y = y
+        _id = id
+    End Sub
+
+    Public Overridable Function Type() As TypesEnum
+        Return TypesEnum.blank
+    End Function
+
+    Public Overridable Sub Go(ByRef player As Players)
+
+    End Sub
+
+End Class
+
+Public Enum TypesEnum
+    blank   'Normal tile
+    jail    'Stuck until another player get's jailed
+    maze    'Go back x tiles
+    goose   'Go x tiles forward
+    inn     'Skip 1 turn
+    death   'Start over
+    start   'Start here
+    finish  'End here
+End Enum
---
+++ trunk/Project challenge ganzenbord/TileInn.vb
@@ -0,0 +1,17 @@
+﻿'Brecht
+Public Class TileInn
+    Inherits Tile
+
+    Public Sub New(ByVal x As Integer, ByVal y As Integer, ByVal id As Integer)
+        MyBase.New(x, y, id)
+    End Sub
+
+    Public Overrides Function Type() As TypesEnum
+        Return TypesEnum.inn
+    End Function
+
+    Public Overrides Sub Go(ByRef player As Players)
+        player.SkipTurn = True
+    End Sub
+
+End Class
