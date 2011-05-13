bccceacff4866f50b56a6b08f8e580101d850523
brecht_derwael@msn.com <brecht_derwael@msn.com@eddbf101-9bcb-924f-d48c-6b5aac6e24a5>
2011-05-13 12:38:53 +0000

    Added Dice.vb

---
+++ Project challenge ganzenbord/Dice.vb
@@ -0,0 +1,34 @@
+﻿Public Class Dice
+    Const ONE = "one.png"
+    Const TWO = "two.png"
+    Const THREE = "three.png"
+    Const FOUR = "four.png"
+    Const FIVE = "five.png"
+    Const SIX = "six.png"
+
+    Private value As Integer
+    Private nums As List(Of String)
+    Private random As Random
+
+    Public Sub Roll(ByRef pic As PictureBox)
+        value = random.Next * 6
+        pic.ImageLocation = nums.Item(value)
+    End Sub
+
+    Public ReadOnly Property DiceValue() As Integer
+        Get
+            Return Value
+        End Get
+    End Property
+
+    Public Sub New()
+        nums = New List(Of String)
+        nums.Add(ONE)
+        nums.Add(TWO)
+        nums.Add(THREE)
+        nums.Add(FOUR)
+        nums.Add(FIVE)
+        nums.Add(SIX)
+        random = New Random()
+    End Sub
+End Class
