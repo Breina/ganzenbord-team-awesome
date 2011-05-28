7ce9e19b0baeac032e278d0e3ec6da1153ac9116
brecht_derwael@msn.com <brecht_derwael@msn.com@eddbf101-9bcb-924f-d48c-6b5aac6e24a5>
2011-05-28 16:30:24 +0000

    New game zo goed as helemaal herschreve

--- trunk/Project challenge ganzenbord/General.vb
+++ trunk/Project challenge ganzenbord/General.vb
@@ -4,4 +4,5 @@
 Module General
     Public player As New List(Of Players)
     Public boolBeginBeurt As Boolean
+
 End Module
--- trunk/Project challenge ganzenbord/NewGame.vb
+++ trunk/Project challenge ganzenbord/NewGame.vb
@@ -3,93 +3,113 @@
 'Aangepast op: 18/05/2011 om 10.14u
 'Nieuw spel 
 Public Class NewGame
-    Private loaded As Boolean = False
-    Private objCreatePlayer() As CreatePlayer
-    Private checked As Boolean
+
     Public lvl As String
-    Private filename As String
+
+    Private previousPlayerCount As Integer = 0
+    Private playerLabels As List(Of Label) = New List(Of Label)             ' "Naam:" label
+    Private playerTextBoxes As List(Of TextBox) = New List(Of TextBox)      ' Name
+    Private playerButtons As List(Of Button) = New List(Of Button)          ' Player colour
+    Private playerCheckBoxes As List(Of CheckBox) = New List(Of CheckBox)   ' For cpu
+
     Public Sub btnStart_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnStart.Click
-        'creeer players
-        Dim i As Int16
-            player.Clear()
-            For i = 0 To CType(nudPlayers.Value - 1, Int16)
-                If objCreatePlayer(i).check.Checked Then
-                    checked = True
-                Else
-                    checked = False
-                End If
-            player.Add(New Players(objCreatePlayer(i).tekst.Text, objCreatePlayer(i).btn.BackColor, checked))
+        For i As Integer = 1 To previousPlayerCount
+            Dim j As Integer = i - 1
+            player.Add(New Players(playerTextBoxes(j).Text, playerButtons(j).BackColor, playerCheckBoxes(j).Checked))
         Next
+
         lvl = TxtLevel.Text
         Me.Hide()
         Form1.Show()
-        boolBeginBeurt = True
     End Sub
 
-    Public Sub playersAanpassen(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles nudPlayers.ValueChanged
-        Dim i As Integer
-        Do Until loaded = True
-            For i = 0 To CType(nudPlayers.Maximum - 1, Int16)
-                Dim txt As New TextBox
-                Dim lbl As New Label
-                Dim btn As New Button
-                Dim check As New CheckBox
-                Randomize()
-                txt.Text = "Player " + (i + 1).ToString
-                check.Text = "Computer"
-                btn.BackColor = Color.FromArgb(CInt(Rnd() * 255), CInt(Rnd() * 255), CInt(Rnd() * 255), CInt(Rnd() * 255))
-                ReDim Preserve objCreatePlayer(i)
-                objCreatePlayer(i) = New CreatePlayer(txt, lbl, btn, i, check)
-                Me.Controls.Add(objCreatePlayer(i).btn)
-                Me.Controls.Add(objCreatePlayer(i).lbl)
-                Me.Controls.Add(objCreatePlayer(i).tekst)
-                Me.Controls.Add(objCreatePlayer(i).check)
-                AddHandler objCreatePlayer(i).btn.Click, AddressOf pickColor
-                AddHandler objCreatePlayer(i).check.CheckedChanged, AddressOf VeranderNaam
+    Public Sub AdjustPlayers(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles NudPlayerCount.ValueChanged
+        If NudPlayerCount.Value > 6 Then
+            NudPlayerCount.Value = 6
+        ElseIf NudPlayerCount.Value < 2 Then
+            NudPlayerCount.Value = 2
+        End If
+
+        Dim playerNum As Integer = CType(NudPlayerCount.Value, Integer) ' DirectCast can't convert Decimal to Integer
+        PlayerBox.Height += (playerNum - previousPlayerCount) * 27
+        Me.Height += (playerNum - previousPlayerCount) * 27
+
+        If NudPlayerCount.Value > previousPlayerCount Then  ' If counted up
+
+            Dim r As Random = New Random()
+            For i As Integer = previousPlayerCount + 1 To playerNum
+
+                playerLabels.Add(New Label)
+                playerTextBoxes.Add(New TextBox)
+                playerButtons.Add(New Button)
+                playerCheckBoxes.Add(New CheckBox)
+
+                Dim s As String = Convert.ToString(i)
+                Dim j = i - 1
+
+                playerLabels(j).Text = "Naam:"
+                playerLabels(j).Name = "LblPlayer" & s
+                playerLabels(j).Width = 40
+                playerLabels(j).Location = New Point(6, 27 * i - 4)
+                playerLabels(j).Visible = True
+
+                playerTextBoxes(j).Text = "Speler " & s
+                playerTextBoxes(j).Name = "TextBoxPlayer" & s
+                playerTextBoxes(j).Location = New Point(50, 27 * i - 7)
+                playerTextBoxes(j).TabIndex = 3 * i - 1
+                playerTextBoxes(j).Visible = True
+
+                playerButtons(j).Text = ""
+                playerButtons(j).Name = "BtnPlayer" & s
+                playerButtons(j).Size = New Size(20, 20)
+                playerButtons(j).BackColor = Color.FromArgb(Convert.ToInt32(r.NextDouble * 127) + 128, _
+                                                            Convert.ToInt32(r.NextDouble * 127) + 128, _
+                                                            Convert.ToInt32(r.NextDouble * 127) + 128)      ' Range 128 to 256 to produce soft colours
+                playerButtons(j).Location = New Point(156, 27 * i - 7)
+                playerButtons(j).TabIndex = 3 * i
+                playerButtons(j).Visible = True
+
+                playerCheckBoxes(j).Text = "Computer"
+                playerCheckBoxes(j).Name = "ChkBoxPlayer" & s
+                playerCheckBoxes(j).Width = 73
+                playerCheckBoxes(j).Location = New Point(182, 27 * i - 8)
+                playerCheckBoxes(j).TabIndex = 3 * i + 1
+                playerCheckBoxes(j).Visible = True
+
+                s = Nothing
+
+                PlayerBox.Controls.Add(playerLabels(j))
+                PlayerBox.Controls.Add(playerTextBoxes(j))
+                PlayerBox.Controls.Add(playerButtons(j))
+                PlayerBox.Controls.Add(playerCheckBoxes(j))
+
+                AddHandler playerButtons(j).Click, AddressOf PickColor
             Next
-            loaded = True
-        Loop
-        For i = 0 To 5
-            objCreatePlayer(i).lbl.Visible = False
-            objCreatePlayer(i).tekst.Visible = False
-            objCreatePlayer(i).btn.Visible = False
-            objCreatePlayer(i).check.Visible = False
-        Next
-        For i = 0 To CType(nudPlayers.Value - 1, Int16)
-            objCreatePlayer(i).lbl.Visible = True
-            objCreatePlayer(i).tekst.Visible = True
-            objCreatePlayer(i).btn.Visible = True
-            objCreatePlayer(i).check.Visible = True
-        Next
+        Else
+            For i As Integer = previousPlayerCount To playerNum + 1 Step -1
+                Dim j = i - 1
+
+                playerLabels(j).Dispose()
+                playerTextBoxes(j).Dispose()
+                playerButtons(j).Dispose()
+                playerCheckBoxes(j).Dispose()
+
+                playerLabels.RemoveAt(j)
+                playerTextBoxes.RemoveAt(j)
+                playerButtons.RemoveAt(j)
+                playerCheckBoxes.RemoveAt(j)
+            Next
+        End If
+
+        previousPlayerCount = playerNum
     End Sub
-    Private Sub pickColor(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    Private Sub PickColor(ByVal sender As System.Object, ByVal e As System.EventArgs)
         ColorDialog1.ShowDialog()
-        Dim btnKleur As Button
-        btnKleur = CType(sender, Button)
-        btnKleur.BackColor = ColorDialog1.Color
-        For i = 0 To 5
-            If btnKleur.Bounds = objCreatePlayer(i).btn.Bounds Then
-                objCreatePlayer(i).btn.BackColor = btnKleur.BackColor
-            End If
-        Next
+        DirectCast(sender, Button).BackColor = ColorDialog1.Color
     End Sub
-    Private Sub VeranderNaam(ByVal sender As System.Object, ByVal e As System.EventArgs)
-        Dim checker As CheckBox
-        Dim computplayer As Integer = 1
-        checker = CType(sender, CheckBox)
-        For i = 0 To CType(nudPlayers.Value - 1, Int16)
-            If objCreatePlayer(i).check.Checked Then
-                objCreatePlayer(i).tekst.Text = "ComputerPlayer" + computplayer.ToString
-                computplayer += 1
-            End If
-        Next
-    End Sub
-    Private Sub btnCancel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCancel.Click
-        Me.Close()
-        Form1.Show()
-    End Sub
+
     Private Sub btnsearch_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnsearch.Click
-   
         Openlvl.InitialDirectory = "\levels"
         Openlvl.Title = "Open a Text File"
         Openlvl.Filter = "Text files (*.txt)|*.txt|" & "All files|*.*"
@@ -107,6 +127,7 @@ Public Class NewGame
             i += 1
             c = Convert.ToChar(Openlvl.FileName.Substring(Openlvl.FileName.Length - i, 1))
         Loop Until c = "\"
+
         c = Nothing
         i = Nothing
 
