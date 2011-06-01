e930ec866b1c2d1765efaa6bc9992e59fc70eb36
brecht_derwael@msn.com <brecht_derwael@msn.com@eddbf101-9bcb-924f-d48c-6b5aac6e24a5>
2011-06-01 15:22:52 +0000

    Added Exception

---
+++ trunk/Project challenge ganzenbord/PathNotFoundException.vb
@@ -0,0 +1,9 @@
+﻿Public Class PathNotFoundException
+    Inherits ApplicationException
+
+    Public Sub New(ByVal message As String)
+        MyBase.New(message)
+        'Form1.AddToChatLog("ERROR: Pad niet gevonden", Color.PaleVioletRed)
+        'continueGame = False
+    End Sub
+End Class
